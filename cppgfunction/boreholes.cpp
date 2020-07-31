ef2f13dc38f0d0356b1ed91b9182f764fd863cbe
j-c-cook <jack.cook@okstate.edu>
2020-07-31 17:29:41 -0500

    boreholes class and methods

--- cppgfunction/boreholes.cpp
+++ cppgfunction/boreholes.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "boreholes.h"
+
+double Distance_Formula(double x1, double y1, double x2, double y2) {
+    return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+}
+
+namespace gt {
+    namespace boreholes {
+
+        double Borehole::distance(Borehole target) {
+            double x1 = x;
+            double y1 = y;
+            double x2 = target.x;
+            double y2 = target.y;
+            double dist = Distance_Formula(x1, y1, x2, y2);
+            return std::max(r_b, double(dist));  // max needs doubles
+        }
+
+        std::tuple<double, double> Borehole::position() {
+            std::tuple<double, double> t (x, y);
+            return t;
+        };
+
+    // ------ Create Fields --------
+    void rectangle_field(std::vector<Borehole>& field, int N_1, int N_2, double B_1, double B_2, double H, double D, double r_b) {
+        int len = field.size(); // check to see if there is enough space in the vector
+        double nbh = N_1 * N_2;
+
+        // TODO: place this resizing into a "general" namespace
+        // if need be, resize the vector to be the same size as the number of boreholes needed
+        if (len != nbh) {
+            field.resize(nbh);
+        } else ; // else do nothing
+
+        double pos_x;
+        double pos_y;
+        int point_nb = 0;
+        for( int j = 0; j < N_2; j++ ) {
+            for (int i = 0; i < N_1; i++) {
+                pos_x = double(i) * B_1;
+                pos_y = double(j) * B_2;
+                field[point_nb] = Borehole(H, D, r_b, pos_x, pos_y);
+                point_nb++;
+            } // end i
+        } // end j
+    } // rectangular field function
+
+    } // boreholes name space
+
+} // gt name space
--- cppgfunction/boreholes.h
+++ cppgfunction/boreholes.h
@@ -5,4 +5,36 @@
 #ifndef CPPGFUNCTION_BOREHOLES_H
 #define CPPGFUNCTION_BOREHOLES_H
 
+#include <iostream>
+#include <math.h>
+#include <tuple>
+#include <vector>
+
+namespace gt {
+    namespace boreholes {
+
+        struct Borehole {
+            // Destructor
+            virtual ~Borehole() {
+            }
+
+            double H;    // height or length of the borehole (meters)
+            double D;    // borehole burial depth (meters)
+            double r_b;  // borehole radius (meters)
+            double x;    // position (meters) of the center of the borehole along the x-axis
+            double y;    // position (meters) of the center of the borehole along the y-axis
+
+            Borehole(double H=0.0, double D=0.0, double r_b=0.0, double x=0.0, double y=0.0) : H(H), D(D), r_b(r_b), x(x), y(y) {
+            }
+
+            double distance(Borehole target);
+            std::tuple<double, double> position();
+        };
+
+        void rectangle_field(std::vector<Borehole>& field, int N_1, int N_2, double B_1, double B_2, double H, double D, double r_b);
+
+    } // boreholes name space
+
+} // gt namespace
+
 #endif //CPPGFUNCTION_BOREHOLES_H
