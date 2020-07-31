932c0daddfb9c93af7beec4dd7db16d17f103980
j-c-cook <jack.cook@okstate.edu>
2020-07-31 17:35:58 -0500

    1D interpolation technique

--- jcc/interpolation.cpp
+++ jcc/interpolation.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "interpolation.h"
+#include <csignal>
+
+using namespace std;
+
+namespace jcc::interpolation {
+
+    double linterp(double xp, double x0, double y0, double x1, double y1) {
+        double yp;
+        yp = y0 + ((y1-y0) / (x1-x0)) * (xp-x0);
+        return yp;
+    } // interp
+
+    double interp1d(vector<double>& xp, vector<double>& yp, vector<double>& x, vector<double>& y) {
+        int counter = 0;
+        for (int i=0; i<yp.size(); i++) {
+            for (int j = counter; j<y.size();j++) {
+                if (xp[i] - x[j] < 10) {
+                    yp[i] = y[j];
+                    break;
+                } else if (xp[i] >= x[j] && xp[i] <= x[j+1]) {
+                    yp[i] = linterp(xp[i], x[j], y[j], x[j+1], y[j+1]);
+                    break;
+                } else if (xp[i] < x[0] || xp[i] > x[x.size()-1]) {
+                    throw invalid_argument( "Need to add extrapolation" );
+                } else {
+                    counter++;
+                } // fi
+            } // next j
+        } // next i
+    } // interp1d
+
+} // jcc::interpolation
+
+
--- jcc/interpolation.h
+++ jcc/interpolation.h
@@ -2,7 +2,19 @@
 // Created by jackcook on 7/15/20.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 #ifndef CPPGFUNCTION_INTERPOLATION_H
 #define CPPGFUNCTION_INTERPOLATION_H
+
+namespace jcc::interpolation {
+
+    double linterp(double xp, double x0, double y0, double x1, double y1);
+    double interp1d(vector<double>& xp, vector<double>& yp, vector<double>& x, vector<double>& y);
+
+} // jcc::interpolation
 
 #endif //CPPGFUNCTION_INTERPOLATION_H
