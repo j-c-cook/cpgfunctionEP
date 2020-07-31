6b7482b99b86ae419fbce5aafe0e775d702bedf2
j-c-cook <jack.cook@okstate.edu>
2020-07-31 17:41:33 -0500

    pool coming from boost lib

--- cppgfunction/pool.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by jackcook on 7/12/20.
-//
-
-#include "pool.h"
--- cppgfunction/pool.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jackcook on 7/12/20.
-//
-
-#ifndef CPPGFUNCTION_POOL_H
-#define CPPGFUNCTION_POOL_H
-
-#endif //CPPGFUNCTION_POOL_H
