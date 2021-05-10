2035815027288978a96249114f1c46471a4b249f
j_c_cook <jack.cook@okstate.edu>
2021-05-10 12:18:04 -0500

    Refactor test 1 to not interfere with test 6 filenames

--- test/configurations.cpp
+++ test/configurations.cpp
@@ -16,32 +16,32 @@ int main() {
 
     // -- Configurations --
     // Get x,y coordinates for a rectangle
-    std::string shape = "Rectangle";
+    std::string shape = "Rectangle_configuration";
     std::vector<std::tuple<double, double>> coordinates =
             gt::coordinates::configuration(shape, Nx, Ny, Bx, By);
     std::string output_path = shape + ".json";
     gt::coordinates::export_coordinates_to_file(coordinates, output_path);
 
     // Get x,y coordinates for an open-rectangle
-    shape = "OpenRectangle";
+    shape = "OpenRectangle_configuration";
     coordinates = gt::coordinates::configuration(shape, Nx, Ny, Bx, By);
     output_path = shape + ".json";
     gt::coordinates::export_coordinates_to_file(coordinates, output_path);
 
     // Get x,y coordinates for a U shape
-    shape = "U";
+    shape = "U_configuration";
     coordinates = gt::coordinates::configuration(shape, Nx, Ny, Bx, By);
     output_path = shape + ".json";
     gt::coordinates::export_coordinates_to_file(coordinates, output_path);
 
     // Get x,y coordinates for a U shape
-    shape = "L";
+    shape = "L_configuration";
     coordinates = gt::coordinates::configuration(shape, Nx, Ny, Bx, By);
     output_path = shape + ".json";
     gt::coordinates::export_coordinates_to_file(coordinates, output_path);
 
     // Read in custom configuration from (.json) file
-    shape = "custom";
+    shape = "custom_configuration";
     std::string input_path = "U.json";
     coordinates = gt::coordinates::configuration(shape, input_path);
     output_path = shape + ".json";
