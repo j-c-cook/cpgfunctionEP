49d5ebf20ff7c36756b39cde42dfb8a09e1c55b2
j_c_cook <jack.cook@okstate.edu>
2021-05-12 14:36:39 -0500

    Remove references to hash table in interpolation

--- include/cpgfunction/interpolation.h
+++ include/cpgfunction/interpolation.h
@@ -17,10 +17,6 @@ namespace jcc { namespace interpolation {
     void interp1d(vector<double>& xp, vector<double>& yp, vector<double>& x, vector<double>& y);
     void interp1d(double &xp, double &yp, vector<double>& x, vector<double>& y);
     void interp1d(double &xp, double &yp, vector<double> &time,
-                  unordered_map<gt::heat_transfer::nKey, double, gt::heat_transfer::KeyHasher> &h_map,
-                  vector<gt::boreholes::Borehole> &boreSegments,
-                  const int i, const int j, const int hash_mode);
-    void interp1d(double &xp, double &yp, vector<double> &time,
                   gt::heat_transfer::SegmentResponse &SegRes, int &i, int &j, int &k);
 
 } } // jcc::interpolation
--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -54,38 +54,6 @@ namespace jcc { namespace interpolation {
         } // next j
     } // interp1d
 
-    void interp1d(double &xp, double &yp, vector<double> &x,
-                  unordered_map<gt::heat_transfer::nKey, double, gt::heat_transfer::KeyHasher> &h_map,
-                   vector<gt::boreholes::Borehole> &boreSegments,
-                  const int i, const int j, const int hash_mode) {
-        if (xp < 0 || xp > x[x.size()-1]) {
-            throw invalid_argument("Need to add extrapolation");
-        }
-        if (0 < xp && xp < x[0]) {
-            double h;
-            gt::heat_transfer::hash_table_lookup(h, h_map, x, boreSegments, i, j, 0, hash_mode);
-            yp = linterp(xp, 0, 0, x[0], h);
-            return;
-        }
-        int counter=0;
-        double h1;
-        gt::heat_transfer::hash_table_lookup(h1, h_map, x, boreSegments, i, j, 0, hash_mode);
-        double h2;
-        gt::heat_transfer::hash_table_lookup(h2, h_map, x, boreSegments, i, j, 1, hash_mode);
-        for (int k=counter; k<x.size()-1; k++) {
-            if (xp>=x[k] && xp <=x[k+1]) {
-                yp = linterp(xp, x[k], h1, x[k+1], h2);
-                return;
-            } else {
-                counter++;
-                h1 = h2;
-                gt::heat_transfer::hash_table_lookup(h2, h_map, x, boreSegments, i, j, counter+1, hash_mode);
-            }
-
-        }
-        int a = 1;
-    }
-
     void interp1d(double &xp, double &yp, vector<double> &time,
                   gt::heat_transfer::SegmentResponse &SegRes, int &i, int &j, int &k) {
         // if the x point is out of bounds, then tell the user that extrapolation is not possible
