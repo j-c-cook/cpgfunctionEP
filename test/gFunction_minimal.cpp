980ba8369ab9e8ffc88aafa9604d573710b9535b
j_c_cook <jack.cook@okstate.edu>
2021-07-13 17:05:15 -0500

    Change spacing of gFctn min and pass n_Threads

--- test/gFunction_minimal.cpp
+++ test/gFunction_minimal.cpp
@@ -40,17 +40,24 @@ int main() {
     // Field of 3x4 (n=12) boreholes
     int n1 = 3;
     int n2 = 4;
+
     // Coordinates
     std::vector<std::tuple<double, double>> coordinates =
             gt::coordinates::rectangle(n1, n2, B, B);
+
     // Move boreholes slightly off of symmetric
     std::get<0>(coordinates[1]) += 2;  // move position 1 borehole x
     std::get<0>(coordinates[2]) -= 1;  // move position 2 borehole x
     std::get<1>(coordinates[2]) -= 1;  // move position 2 borehole y
     std::get<1>(coordinates[3]) -= 1;  // move position 3 borehole y
+
     // Create borehole field
     std::vector<gt::boreholes::Borehole> boreField =
             gt::boreholes::boreField(coordinates, r_b, H, D);
+
+    // Detect number of threads (default uses all available threads)
+    int n_Threads = int(thread::hardware_concurrency());
+    // Compute (and time) the g-Function
     auto start = std::chrono::steady_clock::now();
     std::vector<double> gFunction =
             gt::gfunction::uniform_borehole_wall_temperature(boreField,
@@ -58,7 +65,9 @@ int main() {
                                                              alpha,
                                                              nSegments,
                                                              true,
-                                                             true);
+                                                             true,
+                                                             n_Threads,
+                                                             false);
     auto end = std::chrono::steady_clock::now();
     double seconds = std::chrono::duration_cast<
             std::chrono::milliseconds>(end - start).count() / 1000.;
