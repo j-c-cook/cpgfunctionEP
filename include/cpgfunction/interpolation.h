98a8f82e81613caf8f0e49538177e968b54c3d5f
j_c_cook <jack.cook@okstate.edu>
2021-07-15 14:22:52 -0500

    Maintain gt::heat_transfer to gt::segments C++ 17

--- include/cpgfunction/interpolation.h
+++ include/cpgfunction/interpolation.h
@@ -11,14 +11,14 @@ using namespace std;
 #ifndef CPPGFUNCTION_INTERPOLATION_H
 #define CPPGFUNCTION_INTERPOLATION_H
 
-namespace jcc { namespace interpolation {
+namespace jcc::interpolation {
 
     double linterp(double xp, double x0, double y0, double x1, double y1);
     void interp1d(vector<double>& xp, vector<double>& yp, vector<double>& x, vector<double>& y);
     void interp1d(double &xp, double &yp, vector<double>& x, vector<double>& y);
     void interp1d(double &xp, double &yp, vector<double> &time,
-                  gt::heat_transfer::SegmentResponse &SegRes, int &i, int &j, int &k);
+                  gt::segments::SegmentResponse &SegRes, int &i, int &j, int &k);
 
-} } // jcc::interpolation
+} // jcc::interpolation
 
 #endif //CPPGFUNCTION_INTERPOLATION_H
