cda1ac3764ee148cdb30bde3ed1079ee277d09e1
j_c_cook <jack.cook@okstate.edu>
2021-07-15 13:58:21 -0500

    Add in SegmentResponse object to segments.h/cpp

--- include/cpgfunction/segments.h
+++ include/cpgfunction/segments.h
@@ -2,7 +2,42 @@
 // Created by jackcook on 7/15/21.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 #ifndef CPGFUNCTIONEP_SEGMENTS_H
 #define CPGFUNCTIONEP_SEGMENTS_H
+
+namespace gt::segments {
+
+    struct SegmentResponse {
+        ~SegmentResponse() {} // destructor
+
+        int nSources;
+        int nSum;
+        vector < vector < double > > h_ij;
+        vector<gt::boreholes::Borehole> boreSegments;
+
+        SegmentResponse(int nSources,
+                        int nSum,
+                        int nt) :
+                nSources(nSources),
+                boreSegments(nSources),
+                h_ij(nSum, vector<double>(nt, 0)),
+                nSum(nSum)
+        {} // constructor
+
+        // storage_mode = 1 is the reduced segment response vector
+        int storage_mode = 1;
+
+//        void ReSizeContainers(int n, int nt);
+        void get_h_value(double &h, int i, int j, int k);
+        void get_index_value(int &index, int i, int j);
+    };  // struct SegmentResponse();
+
+
+}
 
 #endif //CPGFUNCTIONEP_SEGMENTS_H
--- src/segments.cpp
+++ src/segments.cpp
@@ -2,3 +2,37 @@
 // Created by jackcook on 7/15/21.
 //
 
+#include <iostream>
+#include <cpgfunction/segments.h>
+
+using namespace std;
+
+namespace gt::segments {
+
+    void SegmentResponse::get_h_value(double &h, const int i, const int j,
+                                      const int k) {
+        int index;
+        switch (storage_mode) {
+            case 0 :
+                cout << "Case 0 not written yet" << endl;
+                break;
+            case 1 :
+                if (i <= j) {
+                    get_index_value(index, i, j);
+                    h = h_ij[index][k];
+                } else {
+                    get_index_value(index, j, i);
+                    h = boreSegments[j].H/boreSegments[i].H * h_ij[index][k];
+                }
+                break;
+            default:
+                throw invalid_argument("The case selected is not currently "
+                                       "implemented.");
+        }  // switch();
+    }  // SegmentResponse::get_h_value();
+
+    void SegmentResponse::get_index_value(int &index, const int i, const int j) {
+        index = i * (2*nSources - i - 1) / 2 + j;
+    }  // SegmentResponse::get_index_value();
+
+}
