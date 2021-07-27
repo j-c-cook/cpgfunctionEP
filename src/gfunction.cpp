a49f5a0a9599c356a07c40ce004bc5726b1340bc
j_c_cook <jack.cook@okstate.edu>
2021-07-27 14:15:11 -0500

    Fixes bug pointed out by Matt Mitchell
    this is the actual fix for #35, the previous fix was only temporary and
    did not resolve the actual bug at hand.
    
    related to #38
    
    Matt makes use of a constant log time expansion of 0.5. The same
    issue Matt was seeing was showing up in the g-function adaptive test
    example. The tests ran for comparisons have made use of Eskilson's
    original 27 time steps. The time difference (delta_t) between the first
    two points were greater than the time in seconds of ln(t/ts)=-8.5.
    There was an if statement in interp1d that had not been hit by any of
    the prior tests. This has been patched.
    
    It also appears that when this case occurred that the load history was
    not properly reconstructed. The load history needed to have a
    reconstruction of p+1, rather than just p.
    
    Thanks, Matt.

--- src/gfunction.cpp
+++ src/gfunction.cpp
@@ -221,9 +221,6 @@ namespace gt::gfunction {
         }  // next i
 
         for (int p=0; p<nt; p++) {
-            if (p==1) {
-                int a = 1;
-            }
             // ------------- fill A ------------
             start = std::chrono::steady_clock::now();
             auto _fillA = [&Hb, &dt, &time, &SegRes, &A]
@@ -254,7 +251,7 @@ namespace gt::gfunction {
             // A needs filled each loop because the _gsl partial pivot
             // decomposition modifies the matrix
             // TODO: Look into reducing the number of times A is built given that Eigen is now being used
-            # pragma omp parallel for num_threads(n_Threads)
+//            # pragma omp parallel for num_threads(n_Threads)
             for (int i=0; i<SIZE; i++) {
                 _fillA(i, p, SIZE);
             }  // next i
@@ -443,7 +440,7 @@ namespace gt::gfunction {
             jcc::interpolation::interp1d(t_reconstructed, yp, t, y);
 
             int idx;
-            for (int j=0; j<p; j++) {
+            for (int j=0; j<p+1; j++) {
                 double c = yp[j];
                 double d = yp[j+1];
                 double e = dt_reconstructed[j];
@@ -507,3 +504,4 @@ namespace gt::gfunction {
         }  // next k
     }  // _temporal_superposition();
 } // namespace gt::gfunction
+            
