ce1301a526e76ef85052776a0c3cad25be8be344
j-c-cook <jack.cook@okstate.edu>
2021-09-02 17:11:24 -0500

    Remove unused files

--- include/stdlib/specfun.h
+++ include/stdlib/specfun.h
@@ -2,7 +2,43 @@
 // Created by jackcook on 9/1/21.
 //
 
+// Mathematical Special Functions for -*- C++ -*-
+
+// Copyright (C) 2006-2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+
 #ifndef CPGFUNCTIONEP_SPECFUN_H
 #define CPGFUNCTIONEP_SPECFUN_H
+
+namespace stdlib {
+
+    template<typename _Tp>
+    inline typename __gnu_cxx::__promote<_Tp>::__type
+    expint(_Tp __x)
+    {
+        typedef typename __gnu_cxx::__promote<_Tp>::__type __type;
+        return __detail::__expint<__type>(__x);
+    }
+
+}
 
 #endif //CPGFUNCTIONEP_SPECFUN_H
